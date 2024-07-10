323637e76866c6f88782b9fc8986db5720f22635
Burgu Shiva Sai <burgushivasai1421@outlook.com>
2024-07-10 20:38:24 +0530

    modifying Helloworld.sh

--- Helloworld.sh
+++ Helloworld.sh
@@ -2,4 +2,4 @@
  person1=Ramesh
  person2=suresh
 echo "$person1:Hii $person2!,How are you?"
-echo "$sperson2:Hii $person1!,I am fine, what about you?"
+echo "$person2:Hii $person1!,I am fine, what about you?"
