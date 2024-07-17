87d9c72da3dcc28b768361319319730dfc96422e
Burgu Shiva Sai <burgushivasai1421@outlook.com>
2024-07-17 13:52:55 +0530

    Editing practice1.sh

--- practice1.sh
+++ practice1.sh
@@ -4,3 +4,7 @@ person1=Ramesh
 person2=suresh
 echo "$person1:Hii $person2! How are you?"
 echo "$person2:Hii $person1! Iam fine! What about you?"
+person1=$1
+person2=$2
+echo "$person1:Hii $person2! How are you?"
+echo "$person2:Hii $person1! Iam fine! What about you?"
