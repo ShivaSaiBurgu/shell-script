f3adb1cd2b5c90cefe6f0fbeb24a3bc0afcd74db
Burgu Shiva Sai <burgushivasai1421@outlook.com>
2024-07-18 23:34:57 +0530

    committing functions.sh

--- functions1.sh
+++ functions1.sh
@@ -6,3 +6,15 @@ echo "Please run the script with the rootb access"
 else
 echo "you are a root user"
 fi
+validate(){
+if [ $1 -ne 0]
+then
+echo "$2..Failure"
+else
+echo "$2...success"
+fi
+}
+dnf install mysql -y
+validate $? "Installing mysql"
+dnf install git -y
+validate $? "Installing git"
