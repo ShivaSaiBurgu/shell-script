e46ca9cfb8418f140813e44985bd30538a72d000
Burgu Shiva Sai <burgushivasai1421@outlook.com>
2024-07-18 11:27:52 +0530

    committing conditions.sh

--- conditions.sh
+++ conditions.sh
@@ -2,9 +2,13 @@
 NUM1=$1
 NUM2=$2
 
-if [ $NUM1 == $NUM2 ]
+if [ $NUM1 -ge $NUM2 ]
 then
-echo "Hii"
-else
-echo "Bye"
+echo "sunday"
+elif [ $NUM1 -le $NUM2]
+then
+echo "Monday"
+elif [ $NUM1 -ne $NUM2]
+then
+echo "Tuesday"
 fi
