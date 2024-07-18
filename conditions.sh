777b94c9e8fc85ecbb7cc92df8e0b74d9b4ecb91
Burgu Shiva Sai <burgushivasai1421@outlook.com>
2024-07-18 13:16:16 +0530

    committing conditions.sh

--- conditions.sh
+++ conditions.sh
@@ -5,10 +5,10 @@ NUM2=$2
 if [ $NUM1 -ge $NUM2 ]
 then
 echo "sunday"
-elif [ $NUM1 -le $NUM2]
+elif [ $NUM1 -le $NUM2 ]
 then
 echo "Monday"
-elif [ $NUM1 -ne $NUM2]
+elif [ $NUM1 -ne $NUM2 ]
 then
 echo "Tuesday"
 fi
