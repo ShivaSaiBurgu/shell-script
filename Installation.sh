0d3509d7fe92af2a5c56101f132dec3a7a3ff969
Burgu Shiva Sai <burgushivasai1421@outlook.com>
2024-07-18 17:31:58 +0530

    committing Installation.sh

--- Installation.sh
+++ Installation.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 USERID=$(id -u)
-if [ $USERID -ne 0 ]
-then
-echo "Please run this script using root user access"
+if [$id -ne 0]
+then 
+echo "Please run the script using root access"
+exit 1
 else
 echo "You are a root user"
-fi
-dnf install mysql -y
-if [ $? -ne 0 ]
-then
-echo "Installation of mysql has failed"
-exit 1
-fi
