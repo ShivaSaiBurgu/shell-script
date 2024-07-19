17047499400b1ccf9f56c5459dd04acbac62de51
Burgu Shiva Sai <burgushivasai1421@outlook.com>
2024-07-19 09:35:00 +0530

    committing functions.sh

--- functions1.sh
+++ functions1.sh
@@ -2,19 +2,8 @@
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
-echo "Please run the script with the rootb access"
+echo "Please run the script with root user access"
+exit 1
 else
 echo "you are a root user"
 fi
-validate(){
-if [ $1 -ne 0 ]
-then
-echo "$2..Failure"
-else
-echo "$2...success"
-fi
-}
-dnf install mysql -y
-validate $? "Installing mysql"
-dnf install git -y
-validate $? "Installing git"
