00468aed5246385ee40bf1e1ef0d058d85f587c1
Burgu Shiva Sai <burgushivasai114@gmail.com>
2025-02-10 23:38:40 +0530

    practicing variables

---
+++ variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=Ramesh
+PERSON2=Suresh
+echo "$PERSON1:: Hii $PERSON2! How are you?"
+echo "$PERSON2:: Hello $PERSON1! Iam Fine! How are you?"
+echo "$PERSON1:: I am fine, How is your work"
+echo "$PERSON2:: Not Bad, Iam thinking to upgrade to DevOps"
