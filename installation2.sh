9f6760e268dd82226351c44457ae91ac086ec155
Burgu Shiva Sai <burgushivasai114@gmail.com>
2025-02-11 19:46:44 +0530

    installation2.sh

---
+++ installation2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+USERTYPE=$(id -u)
+if [ $USERTYPE -ne 0]
+then
+sudo su
+fi
+dnf install mysql -y
+if [ $? -ne 0 ]
+then
+echo "mysql not installed successfully"
+exit 1
+fi
+echo "Installation completed successfully"
