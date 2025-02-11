4f02bde0a590e6241f496a2f651d4dd6e596e8e6
Burgu Shiva Sai <burgushivasai114@gmail.com>
2025-02-11 08:59:10 +0530

    Added READ

---
+++ variables2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1:: Hii $PERSON2! How are you?"
+echo "$PERSON2:: Hello $PERSON1! Iam Fine! How are you?"
+echo "$PERSON1:: I am fine, How is your work"
+echo "$PERSON2:: Not Bad, Iam thinking to upgrade to DevOps"
+echo "Please enter your user name: "
+READ USERNAME
+echo "Please Enter your Password"
+READ PASSWORD
+echo "The Entered details are Username:$USERNAME and password:$PASSWORD"
