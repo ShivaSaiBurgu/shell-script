0344a5aeba3f0a59ccbb0c7d597c7c09c6a7a92b
Burgu Shiva Sai <burgushivasai114@gmail.com>
2025-02-20 21:15:39 +0530

    added functions

--- installation3.sh
+++ installation3.sh
@@ -7,3 +7,13 @@ exit 1
 else
 echo "You are a super user"
 fi
+validate()
+{
+if [ $1 -ne 0 ]
+then 
+echo "git not installed successfully"
+else
+echo "$2"
+}
+yum install git -y
+validate $? installed  git succcessfully
