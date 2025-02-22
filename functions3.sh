0280dae88316951153d46fcc767e188953739567
Burgu Shiva Sai <burgushivasai114@gmail.com>
2025-02-22 17:56:27 +0530

    Added functions3.sh

---
+++ functions3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+USERID=$(id -u)
+if [$USERID -ne 0 ]
+then
+echo "please run the script with use access"
+exit 1
+else
+echo "you are a root user"
+fi
+validate()
+{
+if [ $1 -ne 0 ]
+then
+echo "$2...Failure"
+exit 1
+else
+echo "$2...success"
+fi
+}
+validate $? "Mysql installation"
