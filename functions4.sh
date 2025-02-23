9d12442e821545fa6c3154665b2f65de09033d61
Burgu Shiva Sai <burgushivasai114@gmail.com>
2025-02-23 14:28:09 +0530

    added functions4.sh

--- functions4.sh
+++ functions4.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 Timestamp=$(date +%F-%H-%M-%S)
 script=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$Timestamp-$script.log
+logfile=/tmp/$Timestamp-$script.log
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
@@ -20,9 +20,9 @@ else
 echo "$2...success"
 fi
 }
-yum install git -y &>>$LOGFILE
+yum install git -y &>>$logfile
 validate $? "git installation"
-yum install mysql -y &>>$LOGFILE
+yum install mysql -y &>>$logfile
 validate $? "mysql installation"
-yum install docker -y &>>$LOGFILE
+yum install docker -y &>>$logfile
 validate $? "docker installation"
