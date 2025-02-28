0bdb037f4f5a11dd51604da2740a8327dff6afcf
Burgu Shiva Sai <burgushivasai114@gmail.com>
2025-02-28 18:54:15 +0530

    Added loops.sh

---
+++ loops.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then
+echo "please run the script with root user access"
+else
+echo "you are a root user"
+fi
+for i in $@
+do
+echo "packages to install: $i"
+done
