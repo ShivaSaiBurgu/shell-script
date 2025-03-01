92283187fdfd89cd3b09cf41b8ff20c9c594be32
Burgu Shiva Sai <burgushivasai114@gmail.com>
2025-03-01 12:03:49 +0530

    Added loop.sh

---
+++ loop.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then
+echo "Please run the script with root access"
+exit 1
+else
+echo "You are a root user"
+fi
+time=$(date +%F-%H-%M-%S)
+script=$(echo $0 | cut -d "." -f1)
+LOGFILE=$time-$script.log
+validate()
+{
+    if [ $1 -ne 0 ]
+    then
+    echo "$2...Failure"
+    else
+    echo "$2...success"
+}
+for i in $@
+echo "package to install:$i"
+yum list installed $i &>>LOGFILE
+if [ $? -eq 0 ]
+then
+echo "$i...Already installed"
+else
+yum install $i -y &>>LOGFILE
+validate $? "$i...Installation success"
+fi
