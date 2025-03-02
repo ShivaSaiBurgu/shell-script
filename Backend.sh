a8655c04c11f55e49f35c79c9a435b9a48dab57f
Burgu Shiva Sai <burgushivasai114@gmail.com>
2025-03-02 12:28:07 +0530

    modified Backend.sh

--- Backend.sh
+++ Backend.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+time=$(date +%F-%H-%M-%S)
+script=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$time-$script.log
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
@@ -6,3 +9,20 @@ echo "please run the script with root access"
 else
 echo "You are a root user"
 fi
+
+validate()
+{
+    if [ $1 -ne 0 ]
+    then
+    echo "$2...Failure"
+    exit 1
+    else
+    echo "$2...success"
+    fi
+}
+dnf module disable nodejs -y &>>$LOGFILE
+validate $? "Disabling nodejs"
+dnf module enable nodejs:20 -y &>>$LOGFILE
+validate $? "Enaabling nodejs"
+dnf install nodejs -y &>>$LOGFILE
+validate $? "Installing nodejs"
