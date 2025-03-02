9d04e3f3ae017eac04dd56fc7227c4c12b3f129c
Burgu Shiva Sai <burgushivasai114@gmail.com>
2025-03-02 05:51:41 +0530

    modified mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -1,35 +1,40 @@
 #!/bin/bash
-USERID=$(id -u)
-TIME=$(date +%F-%H-%M-%S)
+time=$(date +%F-%H-%M-%S)
 script=$(echo $0 | cut -d "." -f1)
-LOGFILE=$TIME-$script.log
+LOGFILE=/tmp/$time-$script.log
+USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
-echo "Please run the script with root user"
+echo "please enter the script with root access"
 exit 1
 else
 echo "You are a root user"
 fi
+
 validate()
 {
     if [ $1 -ne 0 ]
     then
-    echo "$2....Failure"
+    echo "$2...Failure"
+    exit 1
     else
     echo "$2...success"
     fi
 }
 dnf install mysql-server -y &>>$LOGFILE
-validate $? "mysql-server-installaion"
-systemctl enable mysqld 
-dnf install mysql-server -y &>>$LOGFILE
-validate $? "enabling mysql-server" 
-dnf install mysql-server -y &>>$LOGFILE
-systemctl start mysqld
-validate $? "starting mysql-server" 
-dnf install mysql-server -y &>>$LOGFILE
-mysql_secure_installation --set-root-pass ExpenseApp@1
-validate $? "password setup"
+validate $? "Installation of mysql"
+systemctl start mysqld &>>$LOGFILE
+validate $? "starting mysql server"
+systemctl enable mysqld &>>$LOGFILE
+validate $? "Enabling mysql server"
+mysql -h localhost -uroot -pExpenseApp@1 -e 'SHOW DATABASES;'
+if [ $? -eq 0 ]
+then
+echo "Mysql Root Password Already setup"
+else
+ mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+ validate $? "Mysql password set up"
+ fi
 
 
 
