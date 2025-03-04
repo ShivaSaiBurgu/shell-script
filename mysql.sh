269785f1355075d2e32c567dc17f86dce6fc797d
Burgu Shiva Sai <burgushivasai114@gmail.com>
2025-03-04 10:31:21 +0530

    modifying mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -7,6 +7,8 @@ exit 1
 else
 echo "You are a root user"
 fi
+echo "please enter the mysql password"
+read -s PWD
 time=$(date +%F-%H-%M-%S)
 script=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$time-$script.log
@@ -25,8 +27,15 @@ systemctl enable mysqld &>>$LOGFILE
 VALIDATE $? "Enabling mysql"
 systemctl start mysqld &>>$LOGFILE
 VALIDATE $? "Starting mysql"
-mysql_secure_installation --set-root-pass ExpenseApp@1 
+mysql -h db.burgu.space -uroot -p${PWD} -e 'show databases;' &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+mysql_secure_installation --set-root-pass ${PWD}
 VALIDATE $? "password setup" 
+else
+echo "password already set up"
+fi
+
 
 
 
