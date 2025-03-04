e422359a19a657bfe9ce5970aa462a0e4e672027
Burgu Shiva Sai <burgushivasai114@gmail.com>
2025-03-04 23:17:42 +0530

    Added mysql2.sh and backend2.sh

--- backend2.sh
+++ backend2.sh
@@ -62,7 +62,7 @@ npm install &>>$LOGFILE
 VALIDATE $? "Installing nodejs dependencies"
 
 #check your repo and path
-cp /home/ec2-user/shell-script/backend2.service /etc/systemd/system/backend2.service &>>$LOGFILE
+cp /home/ec2-user/shell-script/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
 VALIDATE $? "Copied backend service"
 
 systemctl daemon-reload &>>$LOGFILE
