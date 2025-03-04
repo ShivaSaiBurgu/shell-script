7b5b2897e390ebdb0df801e816bbf8ebc89c76d1
Burgu Shiva Sai <burgushivasai114@gmail.com>
2025-03-04 05:53:59 +0530

    rewriting mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -1,42 +1,32 @@
 #!/bin/bash
-time=$(date +%F-%H-%M-%S)
-script=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$time-$script.log
-USERID=$(id -u)
+$USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
-echo "please enter the script with root access"
+echo "Please run the script with root Access"
 exit 1
 else
 echo "You are a root user"
 fi
-echo "please enter mysql root password"
-read -s MYSQLPWD
-
-validate()
+time=$(date +%F-%H-%M-%S)
+script=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$time-$script.log
+VALIDATE()
 {
-    if [ $1 -ne 0 ]
+    if [ $? -ne 0 ]
     then
-    echo "$2...Failure"
-    exit 1
+    echo "$1...Failure"
     else
     echo "$2...success"
     fi
 }
-dnf install mysql-server -y &>>$LOGFILE
-validate $? "Installation of mysql"
-systemctl start mysqld &>>$LOGFILE
-validate $? "starting mysql server"
-systemctl enable mysqld &>>$LOGFILE
-validate $? "Enabling mysql server"
-mysql -h db.burgu.space -uroot -p${MYSQLPWD} -e 'SHOW DATABASES;' &>>LOGFILE
-if [ $? -eq 0 ]
-then
-echo "Mysql Root Password Already setup"
-else
- mysql_secure_installation --set-root-pass ${MYSQLPWD} &>>$LOGFILE
- validate $? "Mysql password set up"
- fi
+yum install mysql -y &>>$LOGFILE
+VALIDATE $? "Installing mysql"
+systemctl enable mysqld
+VALIDATE $? "Enabling mysql"
+systemctl start mysqld
+VALIDATE $? "Starting mysql"
+mysql_secure_installation --set-root-pass ExpenseApp@1
+VALIDATE $? "password setup" 
 
 
 
