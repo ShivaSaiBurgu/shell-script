8331e4d94658e036a36b474859acdf80b00abc70
Burgu Shiva Sai <burgushivasai114@gmail.com>
2026-04-18 11:04:27 +0530

    Adding variables.sh

---
+++ variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Ramesh:Hi,Suresh!How are you?"
+echo "Suresh:Hi,Ramesh, Iam fine. what about you?"
+echo "Ramesh:Iam fine suresh, How is your work?"
+echo "Suresh: Not bad, I am planning to upgrade to DevOps"
