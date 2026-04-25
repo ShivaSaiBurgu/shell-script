ba817d48569e47607dd864d1a8683867daeeeb98
Burgu Shiva Sai <burgushivasai114@gmail.com>
2026-04-25 15:33:06 +0530

    Modified variables.sh

--- variables.sh
+++ variables.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-PERSON1=Ramesh
-PERSON2=Suresh
-echo "$PERSON1:Hi,$PERSON2!How are you?"
-echo "$PERSON2:Hi,$PERSON1, Iam fine. what about you?"
-echo "$PERSON1:Iam fine $PERSON2, How is your work?"
-echo "$PERSON2: Not bad, I am planning to upgrade to DevOps."
+echo "Ramesh:: Hi Suresh!. How are you?"
+echo "Suresh:: Hi Ramesh! I am fine. How are you?"
+echo "Ramesh:: I am fine. What about your work?"
+echo "Suresh:: Not bad!. Iam planning to upgrade to DevOps"
