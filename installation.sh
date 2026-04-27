43ad2f1162c5921e4797b6dd251d9bd7f926c4a9
Burgu Shiva Sai <burgushivasai114@gmail.com>
2026-04-27 12:53:11 +0530

    Added installation.sh

--- installation.sh
+++ installation.sh
@@ -8,3 +8,10 @@ else
 echo "You are a super user"
 fi
 dnf install mysql-server -y
+if [ $? -ne 0 ]
+then
+echo "Installaion of mysql....Failed"
+exit 1
+else
+echo "Installation of mysql....success"
+fi
