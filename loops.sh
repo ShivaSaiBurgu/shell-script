9e7a989b0e1861ecf713f28bcc155e7977d463a8
Burgu Shiva Sai <burgushivasai114@gmail.com>
2026-04-28 20:25:34 +0530

    Added loops.sh

--- loops.sh
+++ loops.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-for i in {1..20}
-do
-echo $i
-done
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -F1)
+echo $SCRIPT_NAME
+if [ $USERID -ne 0 ]
+then
+echo "Please run the script with root user access"
+exit 1
+else
+echo "You are a root user"
+fi
+for i in range
