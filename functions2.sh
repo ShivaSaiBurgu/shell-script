cd417ab13419e545fccc2c9243366c3342d0292c
Burgu Shiva Sai <burgushivasai114@gmail.com>
2026-04-28 13:49:45 +0530

    Added functions2.sh

--- functions2.sh
+++ functions2.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then
+echo "Please run the script with root access"
+exit 1
+else
+echo "You are a super user"
+fi
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$($0 | cut -d "." -f1)
 echo "$TIMESTAMP and $SCRIPT_NAME"
-LOGFILE=/tmp/
 validate() {
 if [ $1 -ne 0 ]
 then
