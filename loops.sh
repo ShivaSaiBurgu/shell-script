be5626d7ac8d50be7e8338062c6590976f3d165e
Burgu Shiva Sai <burgushivasai114@gmail.com>
2026-04-28 20:28:53 +0530

    Added loops.sh

--- loops.sh
+++ loops.sh
@@ -2,6 +2,7 @@
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$TIMESTAMP-$SCRIPT_NAME
 if [ $USERID -ne 0 ]
 then
 echo "Please run the script with root user access"
@@ -9,4 +10,4 @@ exit 1
 else
 echo "You are a root user"
 fi
-echo $SCRIPT_NAME
+
