0810272f6d36ef53cf2d16d0a8d2d4f85dfee20f
Burgu Shiva Sai <burgushivasai114@gmail.com>
2026-04-28 14:37:14 +0530

    Added functions2.sh

--- colours.sh
+++ colours.sh
@@ -8,16 +8,17 @@ else
 echo "You are a super user"
 fi
 R="\e[32m"
+W="\e[0m"
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$TIMESTAMP-$SCRIPT_NAME
 validate() {
 if [ $1 -ne 0 ]
 then
-echo -e "$2... $R failure"
+echo -e "$2... $R failure $W"
 exit 1
 else
-echo -e "$2... $R success"
+echo -e "$2... $R success $W"
 fi
 }
 dnf install mysql -y &>> $LOGFILE
