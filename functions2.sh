592de4c17fa6ef3ad1545063871f54f44ae1e241
Burgu Shiva Sai <burgushivasai114@gmail.com>
2026-04-28 13:50:35 +0530

    Added functions2.sh

--- functions2.sh
+++ functions2.sh
@@ -19,13 +19,6 @@ else
 echo "$2...success"
 fi
 }
-if [ $USERID -ne 0 ]
-then
-echo "Please run the script with root user"
-exit 1
-else
-echo "You are a root user"
-fi
 dnf install mysql -y
 validate $? "Installion of Mysql"
 dnf install git -y
