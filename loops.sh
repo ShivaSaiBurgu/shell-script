c9a3fd41efaf4f721111a3dbae5725ad18d14c6d
Burgu Shiva Sai <burgushivasai114@gmail.com>
2026-04-28 20:36:07 +0530

    Added loops.sh

--- loops.sh
+++ loops.sh
@@ -3,6 +3,15 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$TIMESTAMP-$SCRIPT_NAME
+validate() {
+    if [ $1 -ne 0 ]
+    then
+    echo "$2..Failure"
+    exit 1
+    else
+    echo "$2..success"
+    fi
+}
 if [ $USERID -ne 0 ]
 then
 echo "Please run the script with root user access"
@@ -10,4 +19,6 @@ exit 1
 else
 echo "You are a root user"
 fi
+dnf install mysql -y &>>$LOGFILE
+validate $? "Installation of mysql"
 
