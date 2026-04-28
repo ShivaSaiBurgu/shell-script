bb660b7054921885097eacc157d6617004f8d289
Burgu Shiva Sai <burgushivasai114@gmail.com>
2026-04-28 13:51:31 +0530

    Added functions2.sh

--- functions2.sh
+++ functions2.sh
@@ -8,7 +8,7 @@ else
 echo "You are a super user"
 fi
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$($0 | cut -d "." -f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 echo "$TIMESTAMP and $SCRIPT_NAME"
 validate() {
 if [ $1 -ne 0 ]
