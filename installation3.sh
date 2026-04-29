227eee7a27cdd032ec0bdea51612f12f2fcf4c6f
Burgu Shiva Sai <burgushivasai114@gmail.com>
2026-04-30 00:56:01 +0530

    Added loops.sh

--- installation3.sh
+++ installation3.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/-$TIMESTAMP-$SCRIPT_NAME
 if [ $USERID -ne 0 ]
 then
 echo "Please run the script with root user access"
@@ -7,3 +10,12 @@ exit 1
 else
 echo "You are a root user"
 fi
+for i in $@
+do
+dnf list installed $i
+if [ $? -eq 0 ]
+then
+echo "$i....already installed"
+else
+echo "$i...not installed"
+done
