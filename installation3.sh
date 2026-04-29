b555d9a9a9f53493d65495da00ead57b4638adf7
Burgu Shiva Sai <burgushivasai114@gmail.com>
2026-04-30 01:01:30 +0530

    Added loops.sh

--- installation3.sh
+++ installation3.sh
@@ -3,6 +3,14 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/-$TIMESTAMP-$SCRIPT_NAME
+validate() {
+    if [ $1 -eq 0 ]
+    then
+    echo "$2...success"
+    else
+    echo "$2...Failure"
+    fi
+}
 if [ $USERID -ne 0 ]
 then
 echo "Please run the script with root user access"
@@ -17,6 +25,7 @@ if [ $? -eq 0 ]
 then
 echo "$i....already installed"
 else
-echo "$i...not installed"
+dnf install -y $i
+validate $? "Installation of $i"
 fi
 done
