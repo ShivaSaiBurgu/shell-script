810373bdc42616f8dec34034acd2c3e270a81832
Burgu Shiva Sai <burgushivasai114@gmail.com>
2026-04-30 00:49:31 +0530

    Added loops.sh

--- installation3.sh
+++ installation3.sh
@@ -1,31 +1,9 @@
 #!/bin/bash
 USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$TIMESTAMP-$SCRIPT_NAME
-validate() {
-if [ $1 -ne 0 ]
-then
-echo "$2...Installation failed"
-else
-echo "$2...Installation success"
-fi
-}
 if [ $USERID -ne 0 ]
 then
-echo "Please run the script with root user"
+echo "Please run the script with root user access"
+exit 1
 else
-echo "You are a super user"
+echo "You are a root user"
 fi
-for i in $@
-do
-echo "Installation of $i"
-dnf list installed $id
-if [ $? -eq 0 ]
-then
-echo "$i..already installed"
-else
-dnf install $i -y
-validate $? "Installation of $i"
-fi
-done
