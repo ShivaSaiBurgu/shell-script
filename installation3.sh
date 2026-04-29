f0666c33fb4b8a1dda856bf18aac5baa5668d559
Burgu Shiva Sai <burgushivasai114@gmail.com>
2026-04-29 13:24:12 +0530

    Added loops.sh

--- installation3.sh
+++ installation3.sh
@@ -2,6 +2,7 @@
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$TIMESTAMP-$SCRIPT_NAME
 echo $TIMESTAMP-$SCRIPT_NAME
 if [ $USERID -ne 0 ]
 then
