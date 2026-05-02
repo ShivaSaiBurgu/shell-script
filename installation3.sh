37b55c3778125de5a090189e2af2dd16daac5ec7
Burgu Shiva Sai <burgushivasai114@gmail.com>
2026-05-02 13:35:27 +0530

    modified installation3.sh

--- installation3.sh
+++ installation3.sh
@@ -1,31 +1,9 @@
 #!/bin/bash
 USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/-$TIMESTAMP-$SCRIPT_NAME
-validate() {
-    if [ $1 -eq 0 ]
-    then
-    echo "$2...success"
-    else
-    echo "$2...Failure"
-    fi
-}
 if [ $USERID -ne 0 ]
 then
-echo "Please run the script with root user access"
+echo "please run the script with root access"
 exit 1
 else
-echo "You are a root user"
+echo "You are a super user"
 fi
-for i in $@
-do
-dnf list installed $i &>>$LOGFILE
-if [ $? -eq 0 ]
-then
-echo "$i....already installed"
-else
-dnf install -y $i &>>$LOGFILE
-validate $? "Installation of $i"
-fi
-done
