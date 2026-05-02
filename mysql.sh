8b7daf07171bdad5430830ff5e592fb0de816ad9
Burgu Shiva Sai <burgushivasai114@gmail.com>
2026-05-02 18:06:30 +0530

    Added mysql.sh

---
+++ mysql.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$TIMESTAMP-$SCRIPT_NAME.log
+R="\e[31m"
+G="\e[32m"
+W="\e[0m"
+validate(){
+    if [ $1 -eq 0 ]
+    then
+    echo -e "$2...$G success $W"
+    else
+    echo -e "$2...$R Failure $W"
+    fi
+}
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then
+echo "Please run the script with root user access"
+exit 1
+else
+echo "You are a root user"
+fi
+dnf install mysql-server -y
+validate $? "Installaion of mysql"
