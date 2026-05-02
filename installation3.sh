2b4130d188471d3a0a47b2939aa52bbbbdcd9568
Burgu Shiva Sai <burgushivasai114@gmail.com>
2026-05-02 13:49:56 +0530

    modified installation3.sh

--- installation3.sh
+++ installation3.sh
@@ -7,3 +7,26 @@ exit 1
 else
 echo "You are a super user"
 fi
+validate() {
+    if [ $1 -eq 0 ]
+    then
+    echo "$2....success"
+    else
+    echo "$2...failure"
+    fi
+}
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/-$TIMESTAMP-$SCRIPT_NAME
+R="\e[32m"
+G="\e[33m"
+dnf list installed $@
+for i in $@
+do
+if [ $? -eq 0 ]
+then
+echo "$i...already installed"
+else
+dnf install $i -y
+validate $? "Installation of $i"
+done
