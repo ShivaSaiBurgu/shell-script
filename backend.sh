de0a61ce15cb0080a20ddcdd14a713c95853b2be
Burgu Shiva Sai <burgushivasai114@gmail.com>
2026-05-03 12:03:08 +0530

    modified mysql.sh

---
+++ backend.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$TIMESTAMP-$SCRIPT_NAME.log
+R="\e[31m"
+G="\e[32m"
+W="\e[0m"
+validate(){
+    if [ $1 -ne 0 ]
+    then
+    echo "$2...$R Failure $W"
+    exit 1
+    else
+    echo -e "$2..$G success $W"
+    fi
+}
+if [ $USERID -ne 0 ]
+then
+echo "Please run the script with root access"
+exit 1
+else
+echo "You are a root user"
+fi
+dnf module disable nodejs -y
+validate $? "Disabling nodejs"
+dnf module enable nodejs:20 -y
+validate $? "Enabling nodejs:20"
+dnf install nodejs -y
+validate $? "Installing nodejs"
