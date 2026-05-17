f2fb9a51b84accd9e0c5dd68daac2dc4d144e239
Burgu Shiva Sai <burgushivasai114@gmail.com>
2026-05-17 09:40:11 +0530

    modified mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -26,11 +26,13 @@ systemctl enable mysqld &>>$LOGFILE
 validate $? "Enabling mysql"
 systemctl start mysqld &>>$LOGFILE
 validate $? "Starting mysql"
-mysql -h db.burgu.online -uroot -pExpenseApp@1 -e "SHOW DATABASES;" &>>$LOGFILE
+echo "Please enter mysql password"
+read -s PASSWORD
+mysql -h db.burgu.online -uroot -p${PASSWORD} -e "SHOW DATABASES;" &>>$LOGFILE
 if [ $? -eq 0 ]
 then
 echo "Password already set up"
 else
-mysql_secure_installation --set-root-pass ExpenseApp@1
+mysql_secure_installation --set-root-pass ${PASSWORD}
 validate $? "mysql root password setup"
 fi
