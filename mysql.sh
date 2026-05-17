07ffa04abd35f8932a29e9c99873a74f2e8057bd
Burgu Shiva Sai <burgushivasai114@gmail.com>
2026-05-17 08:12:22 +0530

    modified mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -1,37 +1,8 @@
 #!/bin/bash
 USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$TIMESTAMP-$SCRIPT_NAME.log
-R="\e[31m"
-G="\e[32m"
-W="\e[0m"
-validate(){
-    if [ $1 -ne 0 ]
-    then
-    echo "$2...$R Failure $W"
-    exit 1
-    else
-    echo -e "$2..$G success $W"
-    fi
-}
 if [ $USERID -ne 0 ]
 then
-echo "Please run the script with root access"
-exit 1
+echo "Please run the script with root user access"
 else
-echo "You are a root user"
-fi
-dnf install mysql-server -y &>>$LOGFILE
-validate $? "Installation of mysql"
-systemctl enable mysqld &>>$LOGFILE
-validate $? "Enabling of mysql" 
-systemctl start mysqld &>>$LOGFILE
-validate $? "starting of mysql"
-mysql -h db.burgu.online -uroot -pExpenseApp@1 -e 'SHOW DATABASES;' &>>$LOGFILE
-if [ $? -eq 0 ]
-then
-echo "mysql password already set up...SKipping"
-else
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+echo "you are a root user"
 fi
