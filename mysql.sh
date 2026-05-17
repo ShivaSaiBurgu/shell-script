b2ca6252a7293cd9f1165010990af97d4839fe10
Burgu Shiva Sai <burgushivasai114@gmail.com>
2026-05-17 08:37:48 +0530

    modified mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -2,15 +2,18 @@
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$TIMESTAMP-$SCRIPT_NAME.log
-USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+W="\e[0m"
 validate() {
     if [ $1 -eq 0 ]
     then
-    echo "$2...success"
+    echo -e "$2...$G success $W"
     else
-    echo "$2...Failure"
+    echo -e "$2...$R Failure $W"
     fi
 }
+USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
 echo "Please run the script with root user access"
