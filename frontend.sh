99902298795ee71b3c8caede0610c3b09d734f22
Burgu Shiva Sai <burgushivasai114@gmail.com>
2026-05-17 15:09:27 +0530

    Added Frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -20,20 +20,20 @@ validate() {
     echo -e "$2...$G success $W"
     fi
 }
-dnf install nginx -y 
+dnf install nginx -y &>>$LOGFILE
 validate $? "Installation of nginx"
-systemctl enable nginx
+systemctl enable nginx &>>$LOGFILE
 validate $? "Enabling nginx"
-systemctl start nginx
+systemctl start nginx &>>$LOGFILE
 validate $? "starting nginx"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>$LOGFILE
 validate $? "Removing older content in html"
-curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
 validate $? "Downloading frontend code"
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip &>>$LOGFILE
 validate $? "unzipping code"
-cp home/ec2-user/shell-script/expense.conf /etc/nginx/default.d/expense.conf
+cp home/ec2-user/shell-script/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
 validate $? "copying expen"
-systemctl restart nginx
+systemctl restart nginx &>>$LOGFILE
 validate $? "Restarting nginx"
