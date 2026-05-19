d19da9d415ec9aba255f3e22b446a5320d70fbeb
Burgu Shiva Sai <burgushivasai114@gmail.com>
2026-05-19 09:54:56 +0530

    modified delete-old-files.sh

--- delete-old-files.sh
+++ delete-old-files.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
-set -e
-trap 'echo "error at line ${LINENO}"' ERR
-echo "Process id of current shell script is $$"
-echo "Iam learning DevOps with AWS cloud"
-SOURCE_DIRECTORY=/tmp/app.logs
-find . -name "*.log" -mtime +14
+
+SOURCE_DIRECTORY=/tmp/app-logs
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ -d $SOURCE_DIRECTORY ]
+then
+    echo -e "$G Source directory exists $N"
+else
+    echo -e "$R Please make sure $SOURCE_DIRECTORY exists $N"
+    exit 1
+fi
+
+FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
+
+while IFS= read -r line
+do
+    echo "Deleting file: $line"
+    rm -rf $line
+done <<< $FILES
