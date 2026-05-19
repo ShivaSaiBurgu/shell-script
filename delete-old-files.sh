ed33b159c61a60eb42ce8a37b7ab6f8112bb8fb3
Burgu Shiva Sai <burgushivasai114@gmail.com>
2026-05-19 09:48:13 +0530

    modified delete-old-files.sh

--- delete-old-files.sh
+++ delete-old-files.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
 trap 'echo "error at line ${LINENO}"' ERR
-echoe "Process id of current shell script is $$"
+echo "Process id of current shell script is $$"
 echo "Iam learning DevOps with AWS cloud"
+SOURCE_DIRECTORY=/tmp/app.logs
+find . -name "*.log" -mtime +14
